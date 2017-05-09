c0f12adb122e8282dfeccd90bb2547d11bb81a27
Katey Basye <kbasye@gmail.com>
2017-05-08 22:57:54 -0700

    can add and remove articles

--- backwhere.js
+++ backwhere.js
@@ -1,11 +1,24 @@
 Articles = new Mongo.Collection('articles');
 
+// <---------- CLIENT ---------------->
+
 if(Meteor.isClient){
+
+// Article List
   Template.articles.helpers({
     'article': function(){
-        return Articles.find();
+      return Articles.find( {}, { sort: {created_date: -1} });
     },
 	});
+
+	Template.articles.events({
+		'click .remove': function(){
+			var articleID = this._id;
+			Articles.remove({ _id: articleID });
+		}
+	});
+
+// Add Article
 
 	Template.addArticle.helpers({
     'hiddenClass': function(){
@@ -20,19 +33,30 @@ if(Meteor.isClient){
 	});
 
 	Template.addArticle.events({
-    'click #add-article': function(){
-    	var formIsHidden = Session.get('form');
-    	if(formIsHidden){
-    		Session.set('form', false);
-    		console.log(Session.get('form'));
-    	}
-    	else{
-    		Session.set('form', true)
-    		console.log(Session.get('form'));
-    	} // end if
+    'submit form#add-article': function(e){
+    	e.preventDefault();
+    	Articles.insert({
+    		title: e.target.title.value,
+    		description: e.target.description.value,
+    		city: e.target.city.value,
+    		state: e.target.state.value,
+    		url: e.target.url.value,
+    		cover_photo: e.target.cover_photo.value,
+    		tags: e.target.tags.value,
+    		created_date: Date.now()
+    	})
+			e.target.title.value = "";
+			e.target.description.value = "";
+			e.target.city.value = "";
+			e.target.state.value = "";
+			e.target.url.value = "";
+			e.target.cover_photo.value = "";
+			e.target.tags.value = "";
     }
 	});
 }
+
+// <---------- SERVER ---------------->
 
 if(Meteor.isServer){
   console.log("Server")
