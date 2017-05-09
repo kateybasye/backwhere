b1b5740b75b904704839481783a3ab9e1bdda493
Katey Basye <kbasye@gmail.com>
2017-05-08 21:57:20 -0700

    first commit

--- backwhere.js
+++ backwhere.js
@@ -4,6 +4,32 @@ if(Meteor.isClient){
   Template.articles.helpers({
     'article': function(){
         return Articles.find();
+    },
+	});
+
+	Template.addArticle.helpers({
+    'hiddenClass': function(){
+    	var formIsHidden = Session.get('form');
+    	if (formIsHidden) {
+    		return 'hidden'
+    	}
+    	else {
+    		return ''
+    	}
+    }
+	});
+
+	Template.addArticle.events({
+    'click #add-article': function(){
+    	var formIsHidden = Session.get('form');
+    	if(formIsHidden){
+    		Session.set('form', false);
+    		console.log(Session.get('form'));
+    	}
+    	else{
+    		Session.set('form', true)
+    		console.log(Session.get('form'));
+    	} // end if
     }
 	});
 }
